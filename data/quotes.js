224cb232657bdc0211a9a8fdf107e3ba45e45939
michaelmcguiness <michaelmcguiness123@gmail.com>
2020-09-03 14:37:52 -0400

    added quotes and mostly finished first essay

---
+++ data/quotes.js
@@ -0,0 +1,21 @@
+export default [
+  {
+    quote:
+      "Fall in love with some activity, and do it! Nobody ever figures out what life is all about, and it doesn't matter. Explore the world. Nearly everything is really interesting if you go into it deeply enough.",
+    author: 'Richard Feynman',
+  },
+  {
+    quote:
+      'Study hard what interests you the most in the most undisciplined, irreverent and original manner possible.',
+    author: 'Richard Feynman',
+  },
+  {
+    quote: `If you want to LEARN something well, EXPLAIN it!\n
+      Here are FOUR steps of FEYNMAN technique to learn:\n
+      STEP 1: Choose a concept\n
+      STEP 2: Pretend to teach it to a child\n
+      STEP 3: Identify gaps and go back to the source material\n
+      STEP 4: Review and SIMPLIFY!`,
+    author: 'Richard Feynman',
+  },
+]
